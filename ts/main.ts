6f4630a05bcdb7743ea7d077a6ac13525da62373
FernandoFonseca-code <fernando.fonseca2021@gmail.com>
2024-11-21 11:31:08 -0800

    created pseudo code structure for ts functionality

---
+++ ts/main.ts
@@ -0,0 +1,14 @@
+// add task button will be clicked and the task will be added to the toCompleteListGroup
+
+// first test by printing to the console
+
+// when task is created the task will add a checkbox linked to the task on the left side of the task
+// the text in the enteredText textbox will be cleared and focus back to the enteredText textbox
+
+// when the checkbox is clicked the task will be moved to the completedListGroup list
+// with strikethrough formatting, grayed out, and the checkbox will be checked
+
+
+// create a function that clears completedListGroup list
+
+// add option to store tasks in local storage
